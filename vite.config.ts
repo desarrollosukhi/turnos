17a9a97bc8222c5158349ed02a0b3ea8498015bd
Agustin <agustinyedro@gmail.com>
2026-08-06 11:45:51 -0300

    fix: change base path to root for Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/turnos/',
+  base: '/',
   plugins: [
     vue(),
     vueJsx(),
